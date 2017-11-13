c0b26aa92d0d643c5ce1d41ffbda35bb7c44f679
Arvind Kushwah <arvindkushwah9@gmail.com>
2017-11-13 18:17:00 +0530

    Added new Layout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,7 @@ Peatio::Application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
+  config.serve_static_assets = true
 
   # Use a different cache store in production.
   # config.cache_store = :file_store, "tmp"
